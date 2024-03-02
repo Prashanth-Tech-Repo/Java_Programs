6dee8c25fad513d7fcb7a902cc0fac079fb87989
Prashanth <Prashanth@Prashanth>
2024-03-02 21:42:15 +0530

    Implemented logic for ReverseString class

--- Java_Programs/src/string_Programs/ReverseString.java
+++ Java_Programs/src/string_Programs/ReverseString.java
@@ -1,9 +1,29 @@
 package string_Programs;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String:");
+		String value = sc.nextLine();
+		
+		ReverseString obj = new ReverseString();
+		String reverseValue = obj.reverseString(value);
+		System.out.println(reverseValue);
+		
+		sc.close();
+
+	}
+
+	public String reverseString(String value) {
+		String reverseValue = "";
+		for (int i = value.length() - 1; i >= 0; i--) {
+			reverseValue += value.charAt(i);
+		}
+
+		return reverseValue;
 
 	}
 
